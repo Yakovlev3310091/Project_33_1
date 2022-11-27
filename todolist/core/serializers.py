6c7fde71e0a30bb18fb8b9d9ba8776bb84e2a471
Aleksandr <3310091@mail.ru>
2022-11-27 23:11:08 +0700

    Add user registration

--- todolist/core/serializers.py
+++ todolist/core/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 
 
@@ -13,14 +14,21 @@ class RegistrationSerializer(serializers.ModelSerializer):
         model = USER_MODEL
         fields = "__all__"
 
-    def create(self, validated_data):
-        password = validated_data.get("password")
-        password_repeat = validated_data.pop("password_repeat")
+    def validate(self, attrs):
+        password = attrs.get("password")
+        password_repeat = attrs.pop("password_repeat")
+
+        try:
+            validate_password(password)
+        except Exception as e:
+            raise serializers.ValidationError({"password": e.messages})
 
         if password != password_repeat:
-            raise serializers.ValidationError("Passwords do not match!")
+            raise serializers.ValidationError("Password do not match")
+        return attrs
 
-
+    def create(self, validated_data):
+        password = validated_data.get("password")
         validated_data['password'] = make_password(password)
         instance = super().create(validated_data)
         return instance
