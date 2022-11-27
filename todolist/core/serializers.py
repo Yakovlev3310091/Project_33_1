e94fd8a4e2f87ff8b9e80eca139bded2c4b31349
Aleksandr <3310091@mail.ru>
2022-11-27 14:00:06 +0700

    deploy4

---
+++ todolist/core/serializers.py
@@ -0,0 +1,27 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
+from rest_framework import serializers
+
+
+USER_MODEL = get_user_model()
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    password_repeat = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = USER_MODEL
+        fields = "__all__"
+
+    def create(self, validated_data):
+        password = validated_data.get("password")
+        password_repeat = validated_data.pop("password_repeat")
+
+        if password != password_repeat:
+            raise serializers.ValidationError("Passwords do not match!")
+
+
+        validated_data['password'] = make_password(password)
+        instance = super().create(validated_data)
+        return instance
+
