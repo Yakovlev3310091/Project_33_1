5384d62f9111f6649ed5b40a326758be8f2aa2ba
Aleksandr <3310091@mail.ru>
2022-12-02 20:45:15 +0700

    deploy9

--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('core.urls')),
+    path('core/', include('core.urls')),
     path('oauth', include('social_django.urls', namespace='social')),
     path('goals/', include('goals.urls')),
 
