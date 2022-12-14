04acc59780fa69623d6d3e3757c857f58e2ac2f2
Aleksandr <3310091@mail.ru>
2022-12-14 13:40:00 +0700

    create dc.py

--- todolist/bot/tg/dc.py
+++ todolist/bot/tg/dc.py
@@ -5,15 +5,38 @@ import marshmallow
 import marshmallow_dataclass
 
 
+@dataclass
+class MessageFrom:
+    id: int
+    is_bot: bool
+    first_name: str
+    username: str
+
+    class Meta:
+        unknown = marshmallow.EXCLUDE
 
 
 @dataclass
-class GetUpdatesResponse:
-    ok: bool
-    result: List[UpdateObj]  # todo
+class Chat:
+    id: int
+    username: str
+    first_name: str
+    type: str
 
     class Meta:
-        unknown = EXCLUDE
+        unknown = marshmallow.EXCLUDE
+
+
+@dataclass
+class Message:
+    message_id: int
+    date: int
+    text: str
+    from_: MessageFrom
+    chat: Chat
+
+    class Meta:
+        unknown = marshmallow.EXCLUDE
 
 
 @dataclass
@@ -22,4 +45,26 @@ class SendMessageResponse:
     result: Message  # todo
 
     class Meta:
-        unknown = EXCLUDE
+        unknown = marshmallow.EXCLUDE
+
+
+@dataclass
+class UpdateObj:
+    update_id: int
+    message: Message
+
+    class Meta:
+        unknown = marshmallow.EXCLUDE
+
+
+@dataclass
+class GetUpdatesResponse:
+    ok: bool
+    result: List[UpdateObj]  # todo
+
+    class Meta:
+        unknown = marshmallow.EXCLUDE
+
+
+get_updates_schema = marshmallow_dataclass.class_schema(GetUpdatesResponse)
+send_message_schema = marshmallow_dataclass.class_schema(SendMessageResponse)
